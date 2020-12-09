984cd4b9146341598fc5409e024bfad6e487d14d
付尧 <forzfuyao@gmail.com>
2020-12-09 22:48:57 +0800

    精简代码

--- Week03/cycle/cycle.go
+++ Week03/cycle/cycle.go
@@ -18,25 +18,9 @@ func (c *Cycle) AddServer(s *http.Server) {
 func (c *Cycle) GracefulStop(ctx context.Context) (err error) {
 	var eg errgroup.Group
 	for _, s := range c.servers {
-		ts := s
-		eg.Go(func() error {
-			return ts.Shutdown(ctx)
-		})
-	}
-	return eg.Wait()
-}
-
-func (c *Cycle) runServers() error {
-	var eg errgroup.Group
-	// start multi servers
-	for _, s := range c.servers {
 		s := s
-		eg.Go(func() (err error) {
-			log.Println("服务启动:", s.Addr)
-			defer func() {
-				log.Println("服务关闭:", s.Addr, "err:", err)
-			}()
-			return s.ListenAndServe()
+		eg.Go(func() error {
+			return s.Shutdown(ctx)
 		})
 	}
 	return eg.Wait()
@@ -50,8 +34,20 @@ func (c *Cycle) Stop() (err error) {
 	}
 	return eg.Wait()
 }
-func (c *Cycle) Run() error {
+func (c *Cycle) runServers() error {
 	var eg errgroup.Group
-	eg.Go(c.runServers)
+	// start multi servers
+	for _, s := range c.servers {
+		s := s
+		eg.Go(func() (err error) {
+			log.Println("服务启动:", s.Addr)
+			defer log.Println("服务关闭:", s.Addr, "err:", err)
+			return s.ListenAndServe()
+		})
+	}
 	return eg.Wait()
+}
+
+func (c *Cycle) Run() error {
+	return c.runServers()
 }
